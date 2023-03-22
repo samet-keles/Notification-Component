aa0fc63a631babc2d229513ba442d80f6cfa43cd
Samet <sametkeles@outlook.com>
2023-03-22 13:45:36 +0300

    improved ability to add notification

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <script setup>
+import { ref } from "vue";
 import Notification from "@/components/Notification/Notification.vue";
+
+const notifications = ref([]);
+
+function addNotification() {
+  notifications.value.unshift({
+    type: "success",
+    title: "Notification Title",
+    theme: "dark",
+  });
+}
 </script>
 
 <template>
-  <Notification
-    type="danger"
-    title="Notification Title"
-    theme="dark"
-  ></Notification>
+  <button @click="addNotification">Add Notification</button>
+  <div class="aside">
+    <Notification
+      v-for="(notification, index) in notifications"
+      :key="index"
+      :type="notification.type"
+      :title="notification.title"
+      :theme="notification.theme"
+    ></Notification>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.aside {
+  max-height: 100vh;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+}
+</style>
